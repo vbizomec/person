5bd5fdade6b64484d8ed0a29857319222f415259
Valerija Bizomec <vbizomec@gmail.com>
2022-11-17 17:36:16 +0100

    RestPersonController dodavanje getAll, Delete, Update

--- src/main/java/com/api/person/controller/RestPersonController.java
+++ src/main/java/com/api/person/controller/RestPersonController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 class RestPersonController {
@@ -15,12 +17,63 @@ class RestPersonController {
     private PersonService personService;
 
     @PostMapping("/persons")
-    public ResponseEntity<Person> createPerson(@RequestBody Person person){
+    public ResponseEntity<Person> createPerson(@RequestBody Person person) {
         try {
-           Person person1 = personService.savePerson(new Person(person.getFirstName(), person.getLastName(), person.getAge(), person.getCountry()));
-            return new ResponseEntity<>(person1, HttpStatus.NO_CONTENT);
-        }catch (Exception e) {
+            Person person1 = personService.savePerson(new Person(person.getFirstName(), person.getLastName(), person.getAge(), person.getCountry()));
+            return new ResponseEntity<>(person1, HttpStatus.CREATED);
+        } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/persons")
+    public ResponseEntity<List<Person>> getAllPersons(@RequestParam(required = false) String country) {
+        try {
+
+            List<Person> persons;
+            if (country == null) {
+                persons = personService.getAllPersons();
+            } else {
+                //create a method that will filter by title
+                persons = personService.getPersonByCountry(country);
+            }
+            // get a List of tutorials from database
+            if (persons.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(persons, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/persons/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable("id") long id){
+
+        Person person = personService.getPersonById(id);
+        if(person != null){
+            return  new ResponseEntity<>(person, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/persons/{id}")
+    public ResponseEntity<Person> updateTutorial(@PathVariable("id") long id, @RequestBody Person person){
+
+        Person person1= personService.updatePersonById(id, person);
+        if (person1 !=null){
+            return  new ResponseEntity<>(personService.savePerson(person1), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @DeleteMapping("/persons/{id}")
+    public ResponseEntity<HttpStatus> deletePerson(@PathVariable("id") long id) {
+        //delete one tutorial by id
+        try {
+            personService.deletePersonById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
